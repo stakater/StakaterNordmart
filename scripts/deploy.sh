18a0c751f039b073fefe957f447a5797cd3e44d3
Ahmed Waleed Malik <ahmedwaleedmalik@gmail.com>
2020-02-18 16:14:12 +0500

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,47 +12,47 @@ NAMESPACE_NAME="$NAMESPACE_NAME-$UNIQUE_STRING"
 
 echo "Namespace: $NAMESPACE_NAME"
 
-##Replace DOMAIN placeholder
-#find . -type f -name "*.yaml" -print0 | xargs -0 sed -i "s|DOMAIN|${DOMAIN}|g"
-#find . -type f -name "*.json" -print0 | xargs -0 sed -i "s|DOMAIN|${DOMAIN}|g"
-#
-##Replace NAMESPACE placeholder
-#find . -type f -name "*.yaml" -print0 | xargs -0 sed -i "s|NAMESPACE_NAME|${NAMESPACE_NAME}|g"
-#find . -type f -name "*.json" -print0 | xargs -0 sed -i "s|NAMESPACE_NAME|${NAMESPACE_NAME}|g"
-#sed -i "s|NAMESPACE_NAME|${NAMESPACE_NAME}|g" scripts/destroy.sh
-#
-##Replace KEYCLOAK_CONFIG
-#KEYCLOAK_CONFIG=`cat configs/keycloak.json | base64 -w 0`
-#sed -i "s|KEYCLOAK_CONFIG|${KEYCLOAK_CONFIG}|g" secrets/secret-keycloak-config.yaml
-#
-##Create namespace
-#oc create namespace $NAMESPACE_NAME
-#oc label namespace $NAMESPACE_NAME prometheus=stakater-workload-monitoring
-#
-##Fix permission issue on openshift
-#oc adm policy add-scc-to-user anyuid system:serviceaccount:$NAMESPACE_NAME:default
-#
-##Apply secrets
-#oc apply -f secrets/ --namespace=$NAMESPACE_NAME 2>/dev/null
-#
-##Install kafka
-#oc apply -f kafka.yaml --namespace=$NAMESPACE_NAME
-#
-##Apply manifests
-#n=0
-#until [ $n -ge 2 ]
-#do
-#   oc apply -R -f . --namespace=$NAMESPACE_NAME 2>/dev/null && break
-#   n=$[$n+1]
-#   echo "Retrying for $n/2 times..."
-#done
-#
-#echo "Front-end URL: web-$NAMESPACE_NAME.$DOMAIN"
-#echo "Gateway URL: gateway-$NAMESPACE_NAME.$DOMAIN"
-#
-##CURL gateway to retrieve catalog
-#CATALOG_STATUS=$(curl -X GET -IL https://gateway-$NAMESPACE_NAME.$DOMAIN/api/products 2>/dev/null | head -n 1 | cut -d ' ' -f2)
-#echo "Retrieve catalog status: $CATALOG_STATUS"
-#
-##Change namespace context
-#sudo kubectl config set-context --current --namespace=$NAMESPACE_NAME
+#Replace DOMAIN placeholder
+find . -type f -name "*.yaml" -print0 | xargs -0 sed -i "s|DOMAIN|${DOMAIN}|g"
+find . -type f -name "*.json" -print0 | xargs -0 sed -i "s|DOMAIN|${DOMAIN}|g"
+
+#Replace NAMESPACE placeholder
+find . -type f -name "*.yaml" -print0 | xargs -0 sed -i "s|NAMESPACE_NAME|${NAMESPACE_NAME}|g"
+find . -type f -name "*.json" -print0 | xargs -0 sed -i "s|NAMESPACE_NAME|${NAMESPACE_NAME}|g"
+sed -i "s|NAMESPACE_NAME|${NAMESPACE_NAME}|g" scripts/destroy.sh
+
+#Replace KEYCLOAK_CONFIG
+KEYCLOAK_CONFIG=`cat configs/keycloak.json | base64 -w 0`
+sed -i "s|KEYCLOAK_CONFIG|${KEYCLOAK_CONFIG}|g" secrets/secret-keycloak-config.yaml
+
+#Create namespace
+oc create namespace $NAMESPACE_NAME
+oc label namespace $NAMESPACE_NAME prometheus=stakater-workload-monitoring
+
+#Fix permission issue on openshift
+oc adm policy add-scc-to-user anyuid system:serviceaccount:$NAMESPACE_NAME:default
+
+#Apply secrets
+oc apply -f secrets/ --namespace=$NAMESPACE_NAME 2>/dev/null
+
+#Install kafka
+oc apply -f kafka.yaml --namespace=$NAMESPACE_NAME
+
+#Apply manifests
+n=0
+until [ $n -ge 2 ]
+do
+   oc apply -R -f . --namespace=$NAMESPACE_NAME 2>/dev/null && break
+   n=$[$n+1]
+   echo "Retrying for $n/2 times..."
+done
+
+echo "Front-end URL: web-$NAMESPACE_NAME.$DOMAIN"
+echo "Gateway URL: gateway-$NAMESPACE_NAME.$DOMAIN"
+
+#CURL gateway to retrieve catalog
+CATALOG_STATUS=$(curl -X GET -IL https://gateway-$NAMESPACE_NAME.$DOMAIN/api/products 2>/dev/null | head -n 1 | cut -d ' ' -f2)
+echo "Retrieve catalog status: $CATALOG_STATUS"
+
+#Change namespace context
+sudo kubectl config set-context --current --namespace=$NAMESPACE_NAME
