02ae09dab6b53005a4ed03ed8c449e0a67d43a29
Ahmed Waleed Malik <ahmedwaleedmalik@gmail.com>
2020-02-19 14:21:13 +0500

    refactored script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 DOMAIN="stktrur-200210-1-345785d9ad39a5ed2bf7de019084c0fb-0000.eu-de.containers.appdomain.cloud"
-UNIQUE_STRING=b4mV
+UNIQUE_STRING=$(head -c24 < /dev/random | base64 | head -c 4)
+UNIQUE_STRING="${UNIQUE_STRING,,}"
 OS_NAME=$(uname -s | tr A-Z a-z)
 
 echo "OS NAME: $OS_NAME"
-
-if [[ $OS_NAME == darwin ]]
-then
-  UNIQUE_STRING=$(head -c24 < /dev/random | base64 | head -c 4)
-else
-  UNIQUE_STRING=$(head /dev/urandom | tr -dc a-za-z0-9 | head -c 4)
-fi
 
 NAMESPACE_NAME_TEMP=${1:-nordmart}
 
@@ -24,17 +18,17 @@ NAMESPACE_NAME="$NAMESPACE_NAME-$UNIQUE_STRING"
 echo "Namespace: $NAMESPACE_NAME"
 
 #Replace DOMAIN placeholder
-find . -type f -name "*.yaml" -print0 | xargs -0 sed -i "s|DOMAIN|${DOMAIN}|g"
-find . -type f -name "*.json" -print0 | xargs -0 sed -i "s|DOMAIN|${DOMAIN}|g"
+find . -type f -name "*.yaml" -print0 | xargs -0 sed -i -pe "s|DOMAIN|${DOMAIN}|g"
+find . -type f -name "*.json" -print0 | xargs -0 sed -i -pe "s|DOMAIN|${DOMAIN}|g"
 
 #Replace NAMESPACE placeholder
-find . -type f -name "*.yaml" -print0 | xargs -0 sed -i "s|NAMESPACE_NAME|${NAMESPACE_NAME}|g"
-find . -type f -name "*.json" -print0 | xargs -0 sed -i "s|NAMESPACE_NAME|${NAMESPACE_NAME}|g"
-sed -i "s|NAMESPACE_NAME|${NAMESPACE_NAME}|g" scripts/destroy.sh
+find . -type f -name "*.yaml" -print0 | xargs -0 perl -i -pe "s|NAMESPACE_NAME|${NAMESPACE_NAME}|g"
+find . -type f -name "*.json" -print0 | xargs -0 perl -i -pe "s|NAMESPACE_NAME|${NAMESPACE_NAME}|g"
+perl -i -pe "s|NAMESPACE_NAME|${NAMESPACE_NAME}|g" scripts/destroy.sh
 
 #Replace KEYCLOAK_CONFIG
 KEYCLOAK_CONFIG=`cat configs/keycloak.json | base64 -w 0`
-sed -i "s|KEYCLOAK_CONFIG|${KEYCLOAK_CONFIG}|g" secrets/secret-keycloak-config.yaml
+perl -i -pe "s|KEYCLOAK_CONFIG|${KEYCLOAK_CONFIG}|g" secrets/secret-keycloak-config.yaml
 
 #Create namespace
 oc create namespace $NAMESPACE_NAME
