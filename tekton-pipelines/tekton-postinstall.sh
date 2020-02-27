5a67e2dcfb93225f6e9d004dc97c58fd76a3c8f6
Waleed Malik <ahmedwaleedmalik@gmail.com>
2020-02-27 18:19:43 +0500

    Add basic pipeline for inventory

--- tekton-pipelines/tekton-postinstall.sh
+++ tekton-pipelines/tekton-postinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 oc adm policy add-scc-to-user anyuid -z tekton-pipelines-controller
 sleep 5
-oc get pods --namespace tekton-pipelines --watch
+kubectl get pods --namespace tekton-pipelines --watch
