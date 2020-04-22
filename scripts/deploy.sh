dc3b74fa3e084c671ecec9dd66ebb920b693b1ac
kahootali <kahoot.ali@gmail.com>
2020-04-22 13:54:24 +0500

    change domain

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOMAIN="roks433fk-345785d9ad39a5ed2bf7de019084c0fb-0000.eu-de.containers.appdomain.cloud"
+DOMAIN="roks435stktr-345785d9ad39a5ed2bf7de019084c0fb-0000.eu-de.containers.appdomain.cloud"
 
 read -p "Enter Namespace name: " NAMESPACE_NAME
 
