2a1245ebccb169c5443cec1f16b5849355c56cda
kahootali <kahoot.ali@gmail.com>
2020-06-24 13:18:47 +0500

    update domain to lab cluster

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOMAIN="roks4312stktr-1-345785d9ad39a5ed2bf7de019084c0fb-0000.eu-de.containers.appdomain.cloud"
+DOMAIN="lab-dfa35afe29292c1aaf7804537f813877-0000.osl01.containers.appdomain.cloud"
 
 read -p "Enter Namespace name: " NAMESPACE_NAME
 
